52437ad5ad98698f86f10ef49cdac2472cccc111
cark0s <carlosbe826@gmail.com>
2023-10-13 17:53:22 -0300

    cambios en el main

--- main.js
+++ main.js
@@ -5,12 +5,22 @@ import L from 'leaflet';
 var map = L.map('map').setView([-25.483828, -54.672011], 20);
 
 // Agrega una capa base de OpenStreetMap al mapa
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var openStreetMapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Crea un control de capas
-var layerControl = L.control.layers().addTo(map);
+// Agrega una capa de imágenes de satélite de Mapbox
+var mapboxSatelliteLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2Fya29zIiwiYSI6ImNsbmdvOGU0MzB1cXkyb3BlNGVub3RtNGgifQ.3gOiABlQ3PtDiQe2-KLGjg', {
+    attribution: 'Imágenes de satélite por Mapbox'
+});
+
+// Crea un control de capas y agrega las capas base
+var baseLayers = {
+    "OpenStreetMap": openStreetMapLayer,
+    "Mapbox Satellite": mapboxSatelliteLayer
+};
+
+var layerControl = L.control.layers(baseLayers).addTo(map);
 
 // Define un estilo para tus características
 var myStyle = {
@@ -18,13 +28,15 @@ var myStyle = {
     fillOpacity: 2,      // Opacidad del relleno
     color: 'blue',         // Color del borde
     weight: 2              // Grosor del borde
-  };
-  var myStyle2 = {
+};
+
+var myStyle2 = {
     fillColor: 'blue',    // Color de relleno
     fillOpacity: 2,      // Opacidad del relleno
     color: 'blue',         // Color del borde
     weight: 2              // Grosor del borde
-  };
+};
+
 // Carga y agrega el archivo GeoJSON al mapa
 fetch('https://cdn.glitch.global/e07e2922-47a6-4542-a11a-86e039aa6cbf/Arearecreativas.geojson?v=1697226277657')
     .then(function (response) {
@@ -32,7 +44,7 @@ fetch('https://cdn.glitch.global/e07e2922-47a6-4542-a11a-86e039aa6cbf/Arearecrea
     })
     .then(function (data) {
         var geojsonLayer = L.geoJSON(data, {
-          style: myStyle, // Aplica el estilo al cargar la capa GeoJSON
+            style: myStyle, // Aplica el estilo al cargar la capa GeoJSON
         });
         layerControl.addOverlay(geojsonLayer, 'Area recreativas');
         geojsonLayer.addTo(map);
@@ -44,9 +56,8 @@ fetch('https://cdn.glitch.global/e07e2922-47a6-4542-a11a-86e039aa6cbf/BloqueAula
     })
     .then(function (data) {
         var geojsonLayer = L.geoJSON(data, {
-          style: myStyle2, // Aplica el estilo al cargar la capa GeoJSON
+            style: myStyle2, // Aplica el estilo al cargar la capa GeoJSON
         });
         layerControl.addOverlay(geojsonLayer, 'Bloque de aulas');
         geojsonLayer.addTo(map);
     });
-
